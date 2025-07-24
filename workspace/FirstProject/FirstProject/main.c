fef52c6749efdc38010a97a1a07308226a4111f2
beermoon <Feathermoon0318@gmail.com>
2025-07-24 17:08:59 +0900

    add FirstProject

---
+++ workspace/FirstProject/FirstProject/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  FirstProject
+//
+//  Created by 최명기 on 7/24/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
