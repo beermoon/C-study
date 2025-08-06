60c7263821b7ce491190d93300b884f54acb7a0e
beermoon <Feathermoon0318@gmail.com>
2025-08-06 10:51:39 +0900

    add

---
+++ workspace/format/format/main.c
@@ -0,0 +1,17 @@
+//
+//  main.c
+//  format
+//
+//  Created by 최명기 on 8/6/25.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("hello %d your kid %d",20 ,21);
+    printf("My average grade : %d\n",93.7);
+    printf("My average grade : %f\n",93.7);
+    printf("My average grade : %.1f\n",93.7);
+    return 0;
+}
---
+++ workspace/formatPractice/formatPractice/main.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  formatPractice
+//
+//  Created by 최명기 on 8/6/25.
+//2312
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("We have %d coins in the bank\n",100);
+    printf("We have %f coins in the bank\n",125.7);
+    printf("Year = %d\n",2020);
+    printf("Your average grade is : %f\n",95.13);
+    printf("num1=%d,num2=%d,sum=%d\n",5,7,5+7);
+    printf("num1=%f,num2=%f,sum=%f\n",5.2,7.3,9.5);
+    printf("num1=%d,num2=%d,sub=%d\n",5,3,5-3);
+    printf("a=%d,b=%d,sum=a+b=%d\n",3,5,3+5);
+    printf("a=%d,b=%d,sum=%d+%d=%d\n",3,5,3,5,3+5);
+    printf("a=%d,b=%d,sum=a+b=%d+%d=%d\n",3,5,3,5,3+5);
+    return 0;
+}
